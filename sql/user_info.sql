19de50fee35995d1ddb8883930835e36e3609df6
gglybin <gglybin@gmail.com>
2020-06-03 09:22:11 +0300

    Update user_info.sql

--- sql/user_info.sql
+++ sql/user_info.sql
@@ -38,6 +38,40 @@ from   dba_users
 where  username='&&1';
 
 Prompt ##
+Prompt ## Schema size:
+Prompt ##
+
+col owner for a15
+
+select owner
+       ,round(sum(bytes)/1024/1024,2) as "Size in MB"
+from   dba_segments
+where owner='&&1'
+group  by owner;
+
+Prompt ##
+Prompt ## Top 10 schema objects
+Prompt ##
+
+col segment_name    for a60
+col segment_type    for a30
+col partition_name  for a30
+col tablespace_name for a20
+
+select *
+from   (select owner
+               ,tablespace_name
+               ,segment_type
+               ,segment_name
+               ,partition_name
+               ,round(bytes/(1024*1024),2) SIZE_MB
+        from   dba_segments
+        where  owner='&&1'
+        and    segment_type in ('TABLE', 'TABLE PARTITION', 'TABLE SUBPARTITION','INDEX', 'INDEX PARTITION', 'INDEX SUBPARTITION', 'TEMPORARY', 'LOBINDEX', 'LOBSEGMENT', 'LOB PARTITION')
+        order by bytes desc)
+where rownum <=10;
+
+Prompt ##
 Prompt ## User profile:
 Prompt ##
 
