b83fcbeb3705b19761338b1202422e935077b9b3
gglybin <gglybin@gmail.com>
2020-06-14 23:41:47 +0300

    Update upg_patch_info.sql

--- sql/upg_patch_info.sql
+++ sql/upg_patch_info.sql
@@ -12,12 +12,12 @@ Prompt ##
 Prompt ## Upgrade / Patch History
 Prompt ##
 
-col comments      for a60;
-col action        for a12;
-col version       for a35;
-col namespace     for a10;
-col bundle_series for a15;
-col action_time   for a35;
+col comments      for a60
+col action        for a12
+col version       for a35
+col namespace     for a10
+col bundle_series for a15
+col action_time   for a35
 
 select * 
 from   registry$history 
