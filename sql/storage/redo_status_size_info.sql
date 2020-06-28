4141e749ee7f74b9ff17c01e0bcc1dbce3af8074
Glybin Gennady <gglybin@gmail.com>
2020-06-28 21:39:26 +0300

    Added info about standby redo logs.

--- sql/storage/redo_status_size_info.sql
+++ sql/storage/redo_status_size_info.sql
@@ -20,10 +20,28 @@ break on report
 compute sum label 'Total' of SIZE_MB on report
 
 select a.group#
+       ,b.type
        ,b.member
        ,a.status
        ,(a.bytes / 1024 / 1024) SIZE_MB
 from   gv$log a,
        gv$logfile b
 where  a.group#=b.group#
-order  by 1, 2;
+order  by 1, 3;
+
+Prompt ##
+Prompt ## Standby logs:
+Prompt ##
+
+break on report
+compute sum label 'Total' of SIZE_MB on report
+
+select a.group#
+       ,b.type
+       ,b.member
+       ,a.status
+       ,(a.bytes / 1024 / 1024) SIZE_MB
+from   gv$standby_log a,
+       gv$logfile b
+where  a.group#=b.group#
+order  by 1, 3;
